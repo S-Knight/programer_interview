4d8b91fca150ffd15dba37216198fc9132cdd198
knight <360776816@qq.com>
2020-06-25 15:32:50 +0800

    基本上完成了 设计一个有 getMin 功能的栈

--- src/test/MyStackTest.java
+++ src/test/MyStackTest.java
@@ -14,14 +14,20 @@ public class MyStackTest {
         scanner = new Scanner(System.in);
 
         String userChoice = "";
-        do{
+        do {
             showTips();
             userChoice = scanner.next();
 
             if (userChoice.equals("1")) {
                 push();
+            } else if (userChoice.equals("2")) {
+                pop();
+            } else if (userChoice.equals("3")) {
+                getMin();
+            }else if(!userChoice.equals("q")) {
+                System.out.println("输入错误，请重新输入!");
             }
-        }while (!userChoice.equals("q"));
+        } while (!userChoice.equals("q"));
     }
 
     public void showTips() {
@@ -33,9 +39,21 @@ public class MyStackTest {
     }
 
     public void push() {
-        System.out.println("请输入要push的元素");
+        System.out.println("请输入要push的元素:");
 
         double num = scanner.nextDouble();
         myStack.push(num);
+    }
+
+    public void pop() {
+        double num = myStack.pop();
+
+        System.out.println("pop得到的元素为:" + num);
+    }
+
+    public void getMin() {
+        double num = myStack.getMin();
+
+        System.out.println("getMin得到的元素为:" + num);
     }
 }
